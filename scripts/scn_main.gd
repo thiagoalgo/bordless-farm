b38710364301f1aeca2151d416b05fd4c731a54f
Thiago Goulart <thiagoalgo@gmail.com>
2025-08-09 10:55:28 -0300

    Add side selection to menu and level setup
    Introduces an OptionButton to the menu for selecting a side (North, South, East, West). The selected side is passed to the level setup, allowing the game to start with the chosen side. Refactors menu layout to use containers and updates signal connections accordingly.

--- scripts/scn_main.gd
+++ scripts/scn_main.gd
@@ -1,18 +1,17 @@
 extends Node
 
-var scn_menu := preload("res://scenes/scn_menu.tscn")
+var scn_menu  := preload("res://scenes/scn_menu.tscn")
 var scn_level := preload("res://scenes/scn_level.tscn")
-
-var scn_menu_instance: Node  = null
+var scn_menu_instance: Node = null
 var scn_level_instance: Node = null
-var screen_size: Vector2i    = DisplayServer.screen_get_size()
+var screen_size: Vector2i = DisplayServer.screen_get_size()
+var selected_side: int = 0
 
 
 func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	init_win()
 	open_menu()
-	#open_level()
 
 
 func  init_win() -> void:
@@ -23,23 +22,32 @@ func  init_win() -> void:
 
 func open_menu() -> void:
 	if scn_menu_instance: return
-	
+
 	DisplayServer.window_set_position((screen_size - get_window().size) / 2)
 	scn_menu_instance = scn_menu.instantiate()
 	scn_menu_instance.button_play_pressed.connect(_on_menu_button_play_pressed)
+	scn_menu_instance.option_button_side_item_selected.connect(_on_option_button_side_item_selected)
+	scn_menu_instance.set_z_index(1000)
 	add_child(scn_menu_instance)
 
 
 func open_level() -> void:
-	if scn_level_instance: return
-	
+	#if scn_level_instance: return
+	if scn_level_instance:
+		scn_level_instance.queue_free()
+
 	scn_level_instance = scn_level.instantiate()
-	scn_level_instance.setup(scn_level_instance.GameWindowSide.NORTH, 0, 70)
+	scn_level_instance.setup(selected_side, 0, 70)
 	DisplayServer.window_set_position(scn_level_instance.map.pos)
 	DisplayServer.window_set_size(scn_level_instance.map.size)
 	add_child(scn_level_instance)
 
 
 func _on_menu_button_play_pressed():
-	scn_menu_instance.queue_free()
+	#scn_menu_instance.queue_free()
 	open_level()
+
+
+func _on_option_button_side_item_selected(side: int) -> void:
+	print("Selected side: %d" % side)
+	selected_side = side
--- scripts/scn_menu.gd
+++ scripts/scn_menu.gd
@@ -1,9 +1,14 @@
 extends Control
 
 signal button_play_pressed
+signal option_button_side_item_selected
 
-@onready var button_play: Button = $ButtonPlay
+@onready var button_play: Button = $VBoxContainer/ButtonPlay
+@onready var option_button_side: OptionButton = $VBoxContainer/OptionButtonSide
 
 
 func _ready() -> void:
 	button_play.pressed.connect(func(): emit_signal("button_play_pressed"))
+	option_button_side.item_selected.connect(
+		func(side: int): emit_signal("option_button_side_item_selected", side)
+	)
